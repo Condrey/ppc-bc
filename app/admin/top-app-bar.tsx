c8b45867280fd26b01c36b9b33bd6c22ba32f15e
Condrey <wangzdrey@gmail.com>
2026-04-03 15:18:58 +0300

    fix: adjust text size and alignment in TopAppBar component

--- app/admin/top-app-bar.tsx
+++ app/admin/top-app-bar.tsx
@@ -15,7 +15,7 @@ export default function TopAppBar({ className }: TopAppBarProps) {
         size={"xl"}
         variant={"destructive"}
       />
-      <p className="uppercase ms-2 text-2xl w-full flex items-center ">
+      <p className="uppercase ms-2 text-xl md:text-2xl w-full  items-center inline ">
         <span className="md:hidden">{webName}</span>
         <span className="hidden md:block">{siteConfig.name}</span>
       </p>
